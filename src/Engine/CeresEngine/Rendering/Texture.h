a61b95c6680504e5a489a1b020abd6c3258cd982
PluseggWill <54594803+PluseggWill@users.noreply.github.com>
2020-04-11 19:58:03 -0400

    Upload

---
+++ src/Engine/CeresEngine/Rendering/Texture.h
@@ -0,0 +1,15 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+#pragma once
+
+#include <d3d11.h>
+
+class Texture
+{
+protected:
+	Texture();
+public:
+	~Texture();
+};
+
+#endif // !TEXTURE_H
--- src/Engine/CeresEngine/Rendering/Vertex.h
+++ src/Engine/CeresEngine/Rendering/Vertex.h
@@ -1,6 +1,10 @@
 #pragma once
-
+#include <d3d11.h>
 #include <DirectXMath.h>
+#include <vector>
+#include <Windows.h>
+
+using namespace DirectX;
 
 // --------------------------------------------------------
 // A custom vertex definition
@@ -9,6 +13,30 @@
 // --------------------------------------------------------
 struct Vertex
 {
-	DirectX::XMFLOAT3 Position;	    // The position of the vertex
-	DirectX::XMFLOAT4 Color;        // The color of the vertex
+	// TODO: UV
+	Vertex() {}
+	Vertex(const XMFLOAT3& p, const XMFLOAT3& n, 
+		   const XMFLOAT3& t, const XMFLOAT2& uv)
+		: Position(p), Normal(n), TangentU(t), TexC(uv) {}
+	Vertex(
+		float px, float py, float pz,
+		float nx, float ny, float nz,
+		float tx, float ty, float tz,
+		float u, float v
+		)
+		: Position(px, py, pz), Normal(nx, ny, nz),
+		TangentU(tx, ty, tz), TexC(u,v) {}
+	XMFLOAT3 Position;	    // The position of the vertex
+	//XMFLOAT4 Color;        // The color of the vertex
+	XMFLOAT3 Normal;		// The normal of the vertex
+	XMFLOAT2 TexC;			// The texture of the vertex
+	XMFLOAT3 TangentU;		// The Tangent vector of the vertex
 };
+
+struct MeshData
+{
+	std::vector<Vertex> Vertices;
+	std::vector<UINT> Indices;
+};
+
+
