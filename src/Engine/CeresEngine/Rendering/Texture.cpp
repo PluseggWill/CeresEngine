59501cbf21937391c9c4daf7692a4d691c1949b7
PluseggWill <54594803+PluseggWill@users.noreply.github.com>
2020-04-18 17:11:21 -0400

    update

--- src/Engine/CeresEngine/Rendering/Texture.cpp
+++ src/Engine/CeresEngine/Rendering/Texture.cpp
@@ -1,1 +1,17 @@
 #include "Texture.h"
+
+Texture::Texture()
+{
+	mTexture = NULL;
+}
+
+Texture::~Texture()
+{
+	ReleaseCOM(mTexture);
+}
+
+bool Texture::InitTexture(ID3D11Device* device, LPCSTR filename)
+{
+	D3DX11CreateShaderResourceViewFromFile(device, filename, NULL, NULL, &mTexture, NULL);
+	return true;
+}
--- src/Engine/CeresEngine/Rendering/Texture.h
+++ src/Engine/CeresEngine/Rendering/Texture.h
@@ -3,13 +3,20 @@
 #pragma once
 
 #include <d3d11.h>
+#include "d3dUtil.h"
 
 class Texture
 {
 protected:
-	Texture();
+	ID3D11ShaderResourceView* mTexture;
+
 public:
+	Texture();
 	~Texture();
+
+public:
+	bool InitTexture(ID3D11Device* device, LPCSTR filename);
+	ID3D11ShaderResourceView* GetTexture();
 };
 
 #endif // !TEXTURE_H
